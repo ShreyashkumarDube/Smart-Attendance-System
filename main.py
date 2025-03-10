a7be777092160962803ad384d1e5ea7309f6f2a9
Shreyash Kumar Dube <86827868+ShreyashkumarDube@users.noreply.github.com>
2025-03-10 21:35:04 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,72 @@
-def main():
-    print("Welcome to the Smart Attendance System")
+import os  # os functions
+import check_camera
+import capture_image
+import train_image
+import recognize
+import yagmail
 
-if __name__ == "__main__":
-    main()
-print("Hello World")
+def title_bar():
+    os.system('cls')
+    print("\t***** Face Recognition Attendance System *****")
+
+def mainMenu():
+    title_bar()
+    print()
+    print(10 * "*", "WELCOME MENU", 10 * "*")
+    print("[1] Check Camera")
+    print("[2] Capture Faces")
+    print("[3] Train Images")
+    print("[4] Recognize & Attendance")
+    print("[5] Quit")
+    while True:
+        try:
+            choice = int(input("Enter Choice: "))
+            if choice == 1:
+                check_camera.checkCamera()
+                key = input("Enter any key to return to the main menu")
+                mainMenu()
+                break
+            elif choice == 2:
+                CaptureFaces()
+                break
+            elif choice == 3:
+                Trainimages()
+                break
+            elif choice == 4:
+                recognizeFaces()
+                break
+            elif choice == 5:
+                print("Thank You")
+                break
+            else:
+                print("Invalid Choice. Enter 1-5")
+        except ValueError:
+            print("Invalid Choice. Enter 1-5\n Try Again")
+    exit
+
+# camera test function from check_camera.py file
+def checkCamera():
+    check_camera.checkCamera()
+    key = input("Enter any key to return to the main menu")
+    mainMenu()
+
+# image function from capture_image.py file
+def CaptureFaces():
+    capture_image.takeImages()
+    key = input("Enter any key to return to the main menu")
+    mainMenu()
+
+# train images from train_image.py file
+def Trainimages():
+    train_image.TrainImages()
+    key = input("Enter any key to return to the main menu")
+    mainMenu()
+
+# recognize_attendance from recognize.py file
+def recognizeFaces():
+    recognize.recognize_attendence()
+    key = input("Enter any key to return to the main menu")
+    mainMenu()
+
+# main driver
+mainMenu()
